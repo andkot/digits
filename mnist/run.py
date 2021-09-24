b4132829722b3e078585e020db78e08345dc95b7
VRoman01 <venchak.201130@gmail.com>
2021-09-24 13:08:31 +0300

    for chack

--- mnist/run.py
+++ mnist/run.py
@@ -5,46 +5,41 @@ import numpy as np
 from mnist import MNIST
 
 
+# number of neurons on the first layer
+nn1 = 128
+
+
+def LOSS(X, Y):
+    W1 = np.random.randn(nn1, 784)
+    b1 = np.random.randn(nn1)
+
+    W2 = np.random.randn(11, nn1)
+    b2 = np.random.randn(11)
+
+    l = []
+    for x, y in zip(X, Y):
+        h1 = W1 @ x + b1
+        z1 = np.maximum(0, h1)
+
+        h2 = W2 @ z1 + b2
+        y_p = np.exp(h2) / sum(np.exp(h2))
+
+        dic = {'mse': np.mean((y - y_p) ** 2), 'cross_entropy': np.mean(-y*np.log(y_p))}
+        l.append(dic)
+
+    return l
+
+
 if __name__ == '__main__':
     mndata = MNIST('../python-mnist/data')
     images, labels = mndata.load_training()
 
     X_all = np.array(images) / 255
-    #  Y_all = [5 0 4 1 9...]
-    Y_all = np.array(labels)
-    # Y_all =
-    # [[5 5 5 5 5 5 5 5 5 5 5]
-    #  [0 0 0 0 0 0 0 0 0 0 0]
-    #  [4 4 4 4 4 4 4 4 4 4 4]
-    #  [1 1 1 1 1 1 1 1 1 1 1]
-    #  [9 9 9 9 9 9 9 9 9 9 9]
-    #  ...]
-    Y_all = np.repeat(Y_all, 11).reshape(-1,11)
-    #  Y_all =
-    # [[0 0 0 0 0 1 0 0 0 0 0]
-    #  [1 0 0 0 0 0 0 0 0 0 0]
-    #  [0 0 0 0 1 0 0 0 0 0 0]
-    #  [0 1 0 0 0 0 0 0 0 0 0]
-    #  [0 0 0 0 0 0 0 0 0 1 0]
-    #  ...]
-    Y_all = (Y_all == np.arange(11)).astype(int)
+    Y_all = np.zeros((len(labels), 11))
+    Y_all[range(len(Y_all)), np.array(labels)] = 1
 
     X_train, X_valid, X_test = np.split(X_all, (np.array([0.7, 0.9])*X_all.shape[0]).astype('int32'))
     Y_train, Y_valid, Y_test = np.split(Y_all, (np.array([0.7, 0.9])*Y_all.shape[0]).astype('int32'))
 
-    W1 = np.random.randn(128, 784)
-    b1 = np.random.randn(128)
-
-    W2 = np.random.randn(11, 128)
-    b2 = np.random.randn(11)
-
-    for x, y_t in zip(X_train, Y_train):
-        h1 = W1@x+b1
-        z1 = np.maximum(0, h1)
-
-        h2 = W2@z1 + b2
-        y_p = np.exp(h2)/sum(np.exp(h2))
-
-        loss = np.mean((y_t - y_p)**2)
-
-        print(loss)
+    loss = LOSS(X_train, Y_train)
+    [print(l) for l in loss]
