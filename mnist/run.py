215abfd2c988db1ae573c7a033996a02cd668c9b
VRoman01 <venchak.201130@gmail.com>
2021-09-30 16:04:51 +0300

    for chack

--- mnist/run.py
+++ mnist/run.py
@@ -6,14 +6,14 @@ from mnist import MNIST
 from sklearn.utils import shuffle
 
 # number of neurons on the first layer
-nn1 = 128
+NN1 = 128
 # size output vector
-nno = 11
+NN0 = 11
 # size input vector
-nni = 784
-n_epoch = 10
-h = 0.01
-n_batches = 10
+NNI = 784
+N_EPOCH = 10
+H = 0.01
+N_BATCHES = 10
 
 
 def forward_propagation(x, W1, b1, W2, b2):
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     images, labels = mndata.load_training()
 
     X_all = np.array(images) / 255
-    Y_all = np.zeros((len(labels), nno))
+    Y_all = np.zeros((len(labels), NN0))
     Y_all[range(len(Y_all)), np.array(labels)] = 1
 
     X_train, X_valid, X_test = np.split(X_all, (np.array([0.1, 0.9])*X_all.shape[0]).astype('int32'))
@@ -51,30 +51,33 @@ if __name__ == '__main__':
 
 
     """"""""""""""""""""""""""""""""""""""""""
-    W1 = np.random.randn(nn1, nni)
-    b1 = np.random.randn(nn1)
-    W2 = np.random.randn(nno, nn1)
-    b2 = np.random.randn(nno)
+    W1 = np.random.randn(NN1, NNI)
+    b1 = np.random.randn(NN1)
+    W2 = np.random.randn(NN0, NN1)
+    b2 = np.random.randn(NN0)
 
-    for i in range(n_epoch):
+    list_loss_valid =[]
+    list_loss_train =[]
+
+    for i in range(N_EPOCH):
         X, Y = shuffle(X_train, Y_train)
-        X_batches = np.split(X, n_batches)
-        Y_batches = np.split(Y, n_batches)
+        X_batches = np.split(X, N_BATCHES)
+        Y_batches = np.split(Y, N_BATCHES)
 
         for X, Y in zip(X_batches, Y_batches):
             # массивы производных
-            dW1 = np.zeros((nn1, nni))
-            db1 = np.zeros(nn1)
-            dW2 = np.zeros((nno, nn1))
-            db2 = np.zeros(nno)
+            dW1 = np.zeros((NN1, NNI))
+            db1 = np.zeros(NN1)
+            dW2 = np.zeros((NN0, NN1))
+            db2 = np.zeros(NN0)
 
             # счиатю производные для каждой картинки в батче и добовляю их к массивам произвдодных
             for x, y in zip(X, Y):
                 h1, z1, h2, y_p = forward_propagation(x, W1, b1, W2, b2)
-                db2 = db2 + (y_p - y) / nno
-                dW2 = dW2 + (y_p - y).reshape((-1, 1)) @ z1.reshape((1, -1)) / nno
-                db1 = db1 + (h1 > 0) * ((y_p - y) @ W2) / nno
-                dW1 = dW1 + ((h1 > 0) * ((y_p - y) @ W2)).reshape(-1, 1) @ x.reshape(1, -1) / nno
+                db2 = db2 + (y_p - y) / NN0
+                dW2 = dW2 + (y_p - y).reshape((-1, 1)) @ z1.reshape((1, -1)) / NN0
+                db1 = db1 + (h1 > 0) * ((y_p - y) @ W2) / NN0
+                dW1 = dW1 + ((h1 > 0) * ((y_p - y) @ W2)).reshape(-1, 1) @ x.reshape(1, -1) / NN0
 
             # считаю среднюю производную по всем картинкам в батче
             dW1 = dW1 / X.shape[0]
@@ -83,14 +86,25 @@ if __name__ == '__main__':
             db2 = db2 / X.shape[0]
 
             # изменяю веса по градиентному спуску
-            W1 = W1 - h*dW1
-            b1 = b1 - h*db1
-            W2 = W2 - h*dW2
-            b2 = b2 - h*db2
+            W1 = W1 - H * dW1
+            b1 = b1 - H * db1
+            W2 = W2 - H * dW2
+            b2 = b2 - H * db2
 
+        # это для картинки
+        YP_valid = []
+        for x in X_valid:
+            h1, z1, h2, y_p = forward_propagation(x, W1, b1, W2, b2)
+            YP_valid.append(y_p)
+        list_loss_valid.append(np.mean(loss(Y_valid, YP_valid, 'cross_entropy')))
 
-    # тут я проверил для 2 картинки верно ли выходит - выходит не верно
-    h1, z1, h2, y_p = forward_propagation(X_train[1], W1, b1, W2, b2)
-    print(np.round(y_p, 1))
-    print(Y_train[1])
+        YP_train = []
+        for x in X_train:
+            h1, z1, h2, y_p = forward_propagation(x, W1, b1, W2, b2)
+            YP_train.append(y_p)
+        list_loss_train.append(np.mean(loss(Y_train, YP_train, 'cross_entropy')))
 
+    plt.scatter(range(10), list_loss_valid, label='valid')
+    plt.scatter(range(10), list_loss_train, label='train')
+    plt.legend()
+    plt.show()
--- mnist/test.py
+++ mnist/test.py
@@ -1,44 +1,13 @@
 #!../../venv/bin/python
 import numpy as np
 
-# number of neurons on the first layer
-from run import forward_propagation
+def f(x,y):
+    return y*x
 
-nn1 = 128
-# size output vector
-nno = 11
-# size input vector
-nni = 784
-n_epoch = 10
-h = 0.01
-n_batches = 10
+a = np.array([1,2,3,4])
 
-x = np.random.rand(784)
-y = np.array([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
+b = [0,2,3,4]
 
-W1 = np.random.randn(nn1, nni)
-b1 = np.random.randn(nn1)
-W2 = np.random.randn(nno, nn1)
-b2 = np.random.randn(nno)
+c = f(a , b)
 
-h1, z1, h2, y_p = forward_propagation(x, W1, b1, W2, b2)
-""""""""""""""""""""""""""""""""""""""""""
-dW1 = np.zeros((nn1, nni))
-db1 = np.zeros(nn1)
-dW2 = np.zeros((nno, nn1))
-db2 = np.zeros(nno)
-
-print('--------------------')
-
-dW1 = np.zeros((nn1, nni))
-db1 = np.zeros(nn1)
-dW2 = np.zeros((nno, nn1))
-db2 = np.zeros(nno)
-
-db2 = db2 + (y_p - y)/nno
-dW2 = dW2 + (y_p - y).reshape((-1, 1)) @ z1.reshape((1, -1))/nno
-db1 = db1 + (h1>0)*((y_p-y)@W2)/nno
-dW1 = dW1 + ((h1>0)*((y_p-y)@W2)).reshape(-1,1)@x.reshape(1,-1)/nno
-
-
-print(dW1)
+print(c)
